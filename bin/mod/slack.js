ca4a933144824424dfd5931b1d7085a44ba7b3b6
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-06-01 11:32:08 +0200

    🐼 Using Webhook instead of the bot for slack

--- bin/mod/slack.js
+++ bin/mod/slack.js
@@ -1,4 +1,5 @@
-const discord = require('./discord');
+const Webhook = require('../lib/webhook');
+const { ANNOUNCEMENT_URI } = require('../core/discord.json');
 const store = require('data-store')({ path: process.cwd() + '/bin/core/slack.json' });
 const { slack } = require('../lib/slack');
 
@@ -17,20 +18,23 @@ const fetch = async () => {
         const message_id = m.client_msg_id;
         
         let message = decodeURIComponent(m.text);
-        message = message.replaceAll('<!here>', '').trim();
+        message = message.replaceAll('<!here>', '@here').trim();
+        message = message.replaceAll('<!channel>', '@everyone').trim();
+        message = message.replaceAll('&gt;', ' ').trim();
         
         if (m.type !== "message" || messages_history.includes(message_id)) {
             continue;
         }
-
         const user = await slack.api.users_profile_get(m.user);
            
         messages_history.push(message_id);
         store.set('message_id', messages_history);
 
-        const content = `${message} @everyone [${user.display_name}]`;
-        discord.sendMessage('1107732597011906630', {content})
-       
+        Webhook.send(ANNOUNCEMENT_URI, {
+            username: user.display_name,
+            avatar_url: user.image_original,
+            content: message,
+        })
     }
 
 }
