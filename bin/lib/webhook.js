fbe73d3fbb742510712b52e01a50047f96119e0a
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-06-01 09:35:05 +0000

    🍪 Webhook optimization

--- bin/lib/webhook.js
+++ bin/lib/webhook.js
@@ -1,21 +1,11 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-exports.send = (webhookUrl, payload = {}) => {
-    fetch(webhookUrl, {
+exports.send = async (webhookUrl, payload = {}) => {
+    await fetch(webhookUrl, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify(payload),
-      })
-    .then(response => {
-        if (response.ok) {
-        console.log('Webhook sent successfully.');
-        } else {
-        console.error('Failed to send webhook:', response.status, response.statusText);
-        }
-    })
-    .catch(error => {
-        console.error('Error occurred while sending webhook:', error);
     });
 }
