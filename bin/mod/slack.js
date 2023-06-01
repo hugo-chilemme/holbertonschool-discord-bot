f349bdb06234d46b659814ff3aed3e0ecb31f9e6
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-06-01 14:05:52 +0200

    🏖️ Added ability to edit messages

--- bin/mod/slack.js
+++ bin/mod/slack.js
@@ -10,31 +10,58 @@ const fetch = async () => {
         await slack.api.oauth_refresh();
     }
 
-    const messages = await slack.api.conversations_history('C043TN0J3RD', 1);
+    const WEBHOOK_URI = ANNOUNCEMENT_URI;   
+    const messages = await slack.api.conversations_history('C043TN0J3RD', 100);
     const messages_history = store.get('message_id');
     
     for (const m of messages)
     {
+
         const message_id = m.client_msg_id;
-        
-        let message = decodeURIComponent(m.text);
+        let message = "";
+        try {
+            message = decodeURIComponent(m.text);
+        } catch(e) { }
+
         message = message.replaceAll('<!here>', '@here').trim();
         message = message.replaceAll('<!channel>', '@everyone').trim();
         message = message.replaceAll('&gt;', ' ').trim();
         
-        if (m.type !== "message" || messages_history.includes(message_id)) {
+        if (m.type !== "message") {
+            continue;
+        }
+        
+        if (messages_history[message_id]) {
+            if (m.edited) {
+                const msg = messages_history[message_id];
+                const tts = m.edited.ts;
+
+                if (msg.edited_at === "never" || tts != msg.edited_at) {
+                    await Webhook.edit(msg.webhook_id, msg.message_id, {content: message});
+                    msg.edited_at = tts;
+                    store.set('message_id', messages_history);
+                }
+            }
             continue;
         }
         const user = await slack.api.users_profile_get(m.user);
-           
-        messages_history.push(message_id);
-        store.set('message_id', messages_history);
-
-        Webhook.send(ANNOUNCEMENT_URI, {
+        
+        
+        
+        const messageDiscordId = await Webhook.send(WEBHOOK_URI, {
             username: user.display_name,
             avatar_url: user.image_original,
             content: message,
         })
+        
+        messages_history[message_id] = {
+            webhook_id: WEBHOOK_URI,
+            message_id: messageDiscordId,
+            edited_at: "never",
+        }; 
+
+
+        store.set('message_id', messages_history);
     }
 
 }
