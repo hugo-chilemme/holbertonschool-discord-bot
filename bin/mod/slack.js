8332149db123ae4f06a97890539eb83ccf247a41
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-06-01 16:50:26 +0200

    Add fonctionnaly to delete message on slack

--- bin/mod/slack.js
+++ bin/mod/slack.js
@@ -13,19 +13,41 @@ const fetch = async () => {
     const WEBHOOK_URI = ANNOUNCEMENT_URI;   
     const messages = await slack.api.conversations_history('C043TN0J3RD', 100);
     const messages_history = store.get('message_id');
+
+    let OnlineMessages = [];
+    for (const m of messages) {
+        OnlineMessages.push(m.client_msg_id);
+    }
+
+    let needDeleteEntries = [];
+    for (const client_msg_id of Object.keys(messages_history)) {
+        if (!OnlineMessages.includes(client_msg_id) && client_msg_id !== "undefined") {
+            needDeleteEntries.push(client_msg_id);
+        }
+    }
+    needDeleteEntries.forEach(async ([key, value]) => {
+        await Webhook.remove(value.webhook_id, value.message_id);
+        delete messages_history[key];
+    });
     
+       
     for (const m of messages)
     {
-
         const message_id = m.client_msg_id;
+
+        if (!message_id) {
+            continue;
+        }
+
         let message = "";
         try {
             message = decodeURIComponent(m.text);
         } catch(e) { }
 
-        message = message.replaceAll('<!here>', '@here').trim();
-        message = message.replaceAll('<!channel>', '@everyone').trim();
-        message = message.replaceAll('&gt;', ' ').trim();
+        message = message.replaceAll('<!here>', '@here').trim(); // use message of discord
+        message = message.replaceAll('<!channel>', '@everyone').trim(); // use message of discord
+        message = message.replaceAll('&gt;', ' ').trim(); // Delete 'space' before ul
+        message = message.replaceAll('::', ': :').trim(); // Separate two emojies
         
         if (m.type !== "message") {
             continue;
@@ -39,31 +61,29 @@ const fetch = async () => {
                 if (msg.edited_at === "never" || tts != msg.edited_at) {
                     await Webhook.edit(msg.webhook_id, msg.message_id, {content: message});
                     msg.edited_at = tts;
-                    store.set('message_id', messages_history);
                 }
+            
             }
             continue;
         }
         const user = await slack.api.users_profile_get(m.user);
         
-        
-        
         const messageDiscordId = await Webhook.send(WEBHOOK_URI, {
             username: user.display_name,
             avatar_url: user.image_original,
             content: message,
-        })
+        });
+
         
         messages_history[message_id] = {
             webhook_id: WEBHOOK_URI,
             message_id: messageDiscordId,
+            created_at: m.ts,
             edited_at: "never",
         }; 
-
-
-        store.set('message_id', messages_history);
     }
 
+    store.set('message_id', messages_history);
 }
 
 
