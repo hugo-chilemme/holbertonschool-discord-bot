fa0fd0da0d8fcbf97095f1cdebf55aedfe80c9f5
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-06-01 12:07:57 +0000

    🏍️  Edit process manager

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,13 @@
 const slack = require('./mod/slack');
 const calendar = require('./mod/calendar');
 
-slack.fetch()
-// calendar.fetch();
-
 
 const process = async  () => {
     await slack.fetch()
-    // await calendar.fetch();
+    await calendar.fetch();
 
     setTimeout(process, 5000);
 }
-// setInterval(slack.fetch, 5000);
-// setInterval(calendar.fetch, 5000);
+process();
 
 
